4f65c1463d26a0903ea49db497c3ebf49e35c426
Boba <98185882+BobaDude@users.noreply.github.com>
2025-06-02 13:32:00 -0400

    playround fucntion

--- index.js
+++ index.js
@@ -17,9 +17,50 @@ function getHumanChoice (){
     getHumanChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
 }
 getHumanChoice();
+console.log(getHumanChoice);
+console.log(getComputerChoice);
 
-function playRound (humanScore, computerScore){
-    
-    
+function playRound (humanSelection, computerSelection){
+    //  if((humanSelection == "rock" && computerSelection =="paper")){ 
+    //     return "You lose! The computer chose Paper and you chose Rock." 
+    // } 
+    // else if ((humanSelection ==))
+
+    if (computerSelection  == "rock") {
+        if (humanSelection == "rock") {
+            return "You both chose Rock. It's a tie!"
+        }
+        else if (humanSelection == "paper") {
+            return "You chose Paper and the Computer chose Rock. You Win!"
+        }
+        else {
+            return "You chose Scissors and the Computer chose Rock. You Lose!."
+        }
+    }
+    else if (computerSelection == "paper") {
+        if (humanSelection == "rock") {
+            return "You chose Rock and the Computer chose Paper. You Lose!"
+        }
+        else if (humanSelection == "paper") {
+            return "You both chose Paper. It's a tie!"
+        }
+        else {
+            return "You chose Scissors and the Computer chose Paper. You Win!"
+        }
+    }
+    else if (computerSelection == "scissors"){
+        if (humanSelection == "rock") {
+            return "You chose Rock and the Computer chose Scissors. You Win!"
+    }
+        else if (humanSelection == "paper") {
+            return "You chose Paper and the Computer chose Scissors. You Lose!"
+    }
+        else {
+            return "You both chose Scissors. It's a tie!"
+    }
+}
+}
+
+function playGame() {
     
 }
